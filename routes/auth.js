505ee3a0001497311e34ab2ea252c4e74ec9c898
anoopsingh_1 <as14072002@gmail.com>
2023-07-04 10:30:34 +0530

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,26 @@
+const {
+    login,
+    register,
+    getAllUsers,
+    setAvatar,
+    logOut,
+  } = require("../controllers/userController");
+  
+  const router = require("express").Router();
+  
+  router.post("/login", login);
+  router.post("/register", register);
+  router.get("/allusers/:id", getAllUsers);
+  router.post("/setavatar/:id", setAvatar);
+  router.get("/logout/:id", logOut);
+
+  // import { BASE_URL } from './constants';
+  // const router = require("express").Router();
+  
+  // router.post(`${BASE_URL}/login`, login);
+  // router.post(`${BASE_URL}/register`, register);
+  // router.get(`${BASE_URL}/allusers/:id`, getAllUsers);
+  // router.post(`${BASE_URL}/setavatar/:id`, setAvatar);
+  // router.get(`${BASE_URL}/logout/:id`, logOut);
+  
+  module.exports = router;
---
+++ routes/messages.js
@@ -0,0 +1,11 @@
+const { addMessage, getMessages } = require("../controllers/messageController");
+const router = require("express").Router();
+// import { BASE_URL } from './constants';
+
+router.post("/addmsg/", addMessage);
+router.post("/getmsg/", getMessages);
+
+// router.post(`${BASE_URL}/addmsg/`, addMessage);
+// router.post(`${BASE_URL}/getmsg/`, getMessages);
+
+module.exports = router;
